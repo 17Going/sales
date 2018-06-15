4da1cc9bbb40016d53d1a59497e6a8ed4c96c2c6
ligaoming <gaoming.li@cy-tech.net>
2018-06-15 10:04:30 +0800

    修改人：李高明 修改描述：合入代理配置 修改影响性：无

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api/**': {
+      '/api': {
         target: 'http://118.25.127.172:8080', // 你接口的域名
         // secure: false,      // 如果是https接口，需要配置这个参数
         changeOrigin: true,     // 如果接口跨域，需要进行这个参数配置
