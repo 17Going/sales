245517f69b2ab7e633b1668aa514ff9cba59367a
ligaoming <gaoming.li@cy-tech.net>
2018-06-17 10:33:10 +0800

    修改人：李高明 修改描述：合入网络状态码拦截及处理 修改影响性：无

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
 import store from '@/store'
-import { getToken } from '@/utils/auth'
-import router from '../router'
-import qs from 'qs'
+import { getToken, removeToken } from '@/utils/auth'
+// import router from '@/router'
+// import qs from 'qs'
 
 // create an axios instance
 const service = axios.create({
@@ -68,19 +68,69 @@ service.interceptors.response.use(
     //       return response.data;
     //     }
   },
-  error => {
-    console.log('err' + error)// for debug
-    // 404退出登录去
-    router.push({
-      path: '/login'
-    })
+  err => {
+    if (err && err.response) {
+      switch (err.response.status) {
+        case 400:
+          err.message = '请求错误'
+          break
+
+        case 401:
+          err.message = '未授权，请登录'
+          break
+
+        case 403:
+          err.message = '拒绝访问'
+          break
+
+        case 404:
+          // 404退出登录去 将token清除
+          removeToken()
+          err.message = `请求地址出错: ${err.response.config.url}`
+          setTimeout(() => {
+            this.$store.dispatch('LogOut').then(() => {
+              location.reload()// In order to re-instantiate the vue-router object to avoid bugs
+            })
+          })
+          break
+
+        case 408:
+          err.message = '请求超时'
+          break
+
+        case 500:
+          err.message = '服务器内部错误'
+          break
+
+        case 501:
+          err.message = '服务未实现'
+          break
+
+        case 502:
+          err.message = '网关错误'
+          break
+
+        case 503:
+          err.message = '服务不可用'
+          break
+
+        case 504:
+          err.message = '网关超时'
+          break
+
+        case 505:
+          err.message = 'HTTP版本不受支持'
+          break
+
+        default:
+      }
+    }
     Message({
-      message: error.message,
+      message: err.message,
       type: 'error',
       duration: 5 * 1000
     })
-    // this.$router.push({ path: 'login' })
-    return Promise.reject(error)
+    return Promise.reject(err)
   })
 
 export default service
