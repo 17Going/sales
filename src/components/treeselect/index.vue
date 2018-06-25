823b8b3201a284162a7c2349061b1c40138c74e3
ligaoming <gaoming.li@cy-tech.net>
2018-06-25 14:17:38 +0800

    修改人：李高明 修改描述：合入引入treeselect 修改影响性：无

---
+++ src/components/treeselect/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <treeselect
+    :options="options"
+    :disable-branch-nodes="true"
+    :show-count="true"
+    placeholder="Where are you from?"
+  />
+</template>
+
+<script>
+  // import the component
+  import Treeselect from '@riophae/vue-treeselect'
+  // import the styles
+  import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+  export default {
+    // register the component
+    components: { Treeselect },
+    data() {
+      return {
+        // define default value
+        value: null,
+        // define options
+        options: [{
+          id: 'a',
+          label: 'a',
+          children: [{
+            id: 'aa',
+            label: 'aa'
+          }, {
+            id: 'ab',
+            label: 'ab'
+          }]
+        }, {
+          id: 'b',
+          label: 'b'
+        }, {
+          id: 'c',
+          label: 'c'
+        }]
+      }
+    }
+  }
+</script>
