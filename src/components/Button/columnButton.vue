5a56afcc301a34563f30466425656b54bca92bd2
17701023509@163.com <gaoming.li@cy-tech.net>
2018-07-28 19:16:42 +0800

    修改人：李高明 修改原因：合入button封装 影响分析：无

---
+++ src/components/Button/columnButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-button style="margin-left: 0px"
+             size="mini" :type="type" :icon="icon" plain @click="handleClick">{{text}}</el-button>
+</template>
+
+<script>
+  export default {
+    name: 'column-button',
+    props: {
+      icon: {
+        default: 'el-icon-edit'
+      },
+      text: {
+        default: '录入'
+      },
+      type: {
+        default: 'danger'
+      }
+    },
+    data() {
+      return {
+      }
+    },
+    mounted() {
+
+    },
+    methods: {
+      handleClick(evt) {
+        this.$emit('click', evt);
+      }
+    }
+  }
+</script>
